b60527fd18c1271cecd929054776db3c0cfea768
Maylen Nunez <maylennunez@hotmail.com>
2020-02-09 15:45:02 -0500

    done

---
+++ Opposites-Attract.js
@@ -0,0 +1,14 @@
+function lovefunc(flower1, flower2){
+    // moment of truth
+    
+    
+    if (flower1 % 2===0 && flower2 % 2===0){
+    return false
+    } else if (flower1 % 2 !==0 && flower2 % 2 !==0)
+     {
+      return false
+     } else {
+      return true
+     }
+  lovefunc(6,11);
+  }
